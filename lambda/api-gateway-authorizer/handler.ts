2fe737760187bf460243c223ff080b77e6a3c9a1
Panca Claudiu (Contractor) <claudiu.panca+mcdcorp@us.mcd.com>
2026-02-19 18:56:38 +0200

    fifth commit

--- lambda/api-gateway-authorizer/handler.ts
+++ lambda/api-gateway-authorizer/handler.ts
@@ -13,40 +13,41 @@ export const handler = async (
   event: APIGatewayEvent,
   context: Context
 ): Promise<{ isAuthorized: boolean }> => {
-  //   console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-  //   console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  // console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  // console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+  console.log('BRAND NEW UPLOADED');
 
-  // const headers = event.headers || {};
-  // const authHeader = headers['authorization'] || headers['Authorization'];
-  // console.log(`Authorization header: ${authHeader}`);
-  // const jwtToken = authHeader?.split(' ')[1] || '';
-  // console.log(`Extracted JWT token: ${jwtToken}`);
+  const headers = event.headers || {};
+  const authHeader = headers['authorization'] || headers['Authorization'];
+  console.log(`Authorization header: ${authHeader}`);
+  const jwtToken = authHeader?.split(' ')[1] || '';
+  console.log(`Extracted JWT token: ${jwtToken}`);
 
-  // const decoded = jwt.verify(jwtToken, 'my-secret-key');
-  // console.log('decoded', decoded);
+  const decoded = jwt.verify(jwtToken, 'my-secret-key');
+  console.log('decoded', decoded);
 
-  // const secretKey = await fetchPublicKeySecret();
-  // console.log('secretKey here', secretKey);
+  const secretKey = await fetchPublicKeySecret();
+  console.log('secretKey here', secretKey);
 
-  // const secretString = JSON.parse(
-  //   secretKey?.SecretString || ''
-  // ).PUBLIC_TOKEN_KEY;
+  const secretString = JSON.parse(
+    secretKey?.SecretString || ''
+  ).PUBLIC_TOKEN_KEY;
 
-  // console.log('secretString', secretString);
+  console.log('secretString', secretString);
 
-  const expiresIn = '3h';
-  const jwtToken = jwt.sign({ foo: 'hello world' }, 'my-secret-key', {
-    expiresIn,
-    algorithm: 'HS256',
-  });
-  console.log('jwtToken', jwtToken);
-  return { isAuthorized: true };
+  // const expiresIn = '3h';
+  // const jwtToken = jwt.sign({ foo: 'hello world' }, 'my-secret-key', {
+  //   expiresIn,
+  //   algorithm: 'HS256',
+  // });
+  // console.log('jwtToken', jwtToken);
+  // return { isAuthorized: true };
 
-  // if (decoded) {
-  //   return { isAuthorized: true };
-  // } else {
-  //   return { isAuthorized: false };
-  // }
+  if (decoded) {
+    return { isAuthorized: true };
+  } else {
+    return { isAuthorized: false };
+  }
 
   // return {
   //   statusCode: 200,
