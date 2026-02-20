3574bdef51324fdf6786015076220773612263d7
Clau <claudiu.panca@gmail.com>
2026-02-21 00:41:26 +0200

    prisma

--- lambda/rds-lambda/handler.ts
+++ lambda/rds-lambda/handler.ts
@@ -48,44 +48,31 @@ export const handler = async (event: APIGatewayEvent, context: Context) => {
     //     // Fetch all users with their posts
     //     allUsers = await prisma.users.findMany({});
     //     console.log("All users:", JSON.stringify(allUsers, null, 2));
-    // } catch (error) {
-    //     console.error('Error creating user:', error);
+    // }
+    // try {
+
+    //     main()
+    //         .then(async () => {
+    //             await prisma.$disconnect();
+    //         })
+    //         .catch(async (e) => {
+    //             console.error(e);
+    //             await prisma.$disconnect();
+    //             process.exit(1);
+    //         });
+
+    //     return {
+    //         statusCode: 200,
+    //         body: allUsers
+    //     }
+    // }
+    // catch (e) {
+    //     console.log('Error', e)
+
+    //     return {
+    //         statusCode: 500,
+    //         body: JSON.stringify({ error: e })
+    //     }
     // }
 
-
-    async function main() {
-        // Create a new user with a post
-        const user = await prisma.users.create({
-            data: {
-                name: eventBody?.name,
-                email: eventBody?.email,
-            },
-
-        });
-        console.log("Created user:", user);
-
-        // Fetch all users with their posts
-        allUsers = await prisma.users.findMany({});
-        console.log("All users:", JSON.stringify(allUsers, null, 2));
-    }
-    try {
-        await main();
-        await prisma.$disconnect();
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(allUsers)
-        };
-    }
-    catch (e) {
-        console.error('Error', e);
-        await prisma.$disconnect();
-
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: e })
-        };
-    }
-
 }
-```
